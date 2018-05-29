41c7971cc41642a9bfe875339f37229697f61ce8
sasha de laet <sasha.delaet@gmail.com>
2018-05-29 19:23:50 +0200

    created the complete giphy app

---
+++ src/components/gif-list.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import Gif from './gif';
+
+class GifList extends Component {
+
+  render(){
+    return(
+      <div className="gif-list">
+        {this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={this.props.selectGif}/>)}
+      </div>
+    );
+  }
+}
+
+export default GifList;
